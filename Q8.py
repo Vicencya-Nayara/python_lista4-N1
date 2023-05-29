43de0ec2cde378a8d5c9a3b680f47ecbac1920ce
Vicencya-Nayara <115300537+Vicencya-Nayara@users.noreply.github.com>
2023-05-29 14:13:08 -0300

    Add files via upload

---
+++ Q8.py
@@ -0,0 +1,42 @@
+class Item:
+    def __init__(self, valor):
+        self.valor = valor
+        self.proximo = None
+
+class ListaEncadeada:
+    def __init__(self):
+        self.inicio = None
+        self.tamanho = 0
+
+    def vazia(self):
+        return self.inicio is None
+
+    def inserir_inicio(self, valor):
+        novo_item = Item(valor)
+        novo_item.proximo = self.inicio
+        self.inicio = novo_item
+        self.tamanho += 1
+
+    def exibir_lista(self):
+        if self.vazia():
+            print("A lista está vazia!")
+        else:
+            atual = self.inicio
+            while atual is not None:
+                print(atual.valor, end=" ")
+                atual = atual.proximo
+            print()
+
+def criar_lista_inversa(numeros):
+    lista = ListaEncadeada()
+    for numero in (numeros):
+        lista.inserir_inicio(numero)
+    return lista
+
+numeros = input("Digite os números inteiros separados por espaço: ").split()
+numeros = [int(numero) for numero in numeros]
+numeros.reverse()
+lista_inversa = criar_lista_inversa(numeros)
+
+print("Lista encadeada inversa:")
+lista_inversa.exibir_lista()
